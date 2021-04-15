b6d768e05f886c15948817c650567d3376dbe37e
michael-owen-01 <80764250+michael-owen-01@users.noreply.github.com>
2021-04-15 23:58:58 +0700

    Update BlastBurn.java

--- src/com/engimon/model/skill/BlastBurn.java
+++ src/com/engimon/model/skill/BlastBurn.java
@@ -3,6 +3,7 @@ package com.engimon.model.skill;
 public class BlastBurn extends Skill{
 	public BlastBurn(){
 		super(1,"Blast Burn",100,1);
-		prereqElmt.add(new Fire())
+		prereqElmt.add(new Fire());
 	}
+}
 
