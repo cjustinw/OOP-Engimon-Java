125892ff024b873c491b530969f5c5b1e72fc7fc
Josegalbraith <59763351+Josegalbraith@users.noreply.github.com>
2021-04-16 21:04:19 +0700

    Update Inventory

--- src/com/engimon/model/Inventory/Inventory.java
+++ src/com/engimon/model/Inventory/Inventory.java
@@ -1,36 +1,36 @@
-//package com.engimon.model.inventory;
-//
-//import java.util.*;
-//
-//class Inventory<T> {
-//    private int numOfElement;
-//    private ArrayList<T> myinventory;
-//
-//    public Inventory(){
-//        myinventory = new ArrayList<T>();
-//        numOfElement = 0;
-//    }
-//
-//    protected void finalize(){
-//        myinventory.clear()
-//    }
-//    
-//    public void add(T elmt){
-//        myinventory.add(elmt);
-//        numOfElement += 1;
-//    }
-//
-//    public void delAt(int idx){
-//        if (numOfElement == 0){
-//            // throw empty
-//        }
-//        else{
-//            myinventory.remove(idx);
-//            numOfElement -= 1;
-//        }
-//    }
-//
-//    public int getNumOfElement(){
-//        return numOfElement;
-//    }
-//}
+package com.engimon.model.inventory;
+
+import java.util.*;
+
+class Inventory<T> {
+    private ArrayList<T> myinventory;
+    private int numOfElement;
+    Inventory(){
+        this.myinventory = new ArrayList<>();
+        numOfElement = 0;
+    }
+
+ 
+    // protected void finalize(){
+    // }
+    
+    public void add(T elmt){
+        this.myinventory.add(elmt);
+        numOfElement += 1;
+    }
+ 
+    public void delAt(int idx){
+        if (numOfElement == 0){
+            System.out.println("cannot delete, empty inventory");
+        }
+        else{
+            System.out.println("deleted 1 item");
+            this.myinventory.remove(idx);
+            this.numOfElement -= 1;
+        }
+    }
+ 
+    public int getNumOfElement(){
+        return this.numOfElement;
+    }
+ }
